f003bf8e38aa20c06722bdfdacd22904ee365442
Vidisha Gupta <vidisha.gupta@knoldus.in>
2018-01-11 21:04:06 +0530

    ScalaAssignment01-4 submitted

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -12,15 +12,15 @@ object Application extends App {
   val inputNumber2 = 4
 
   val element1 = 1
-  val element2 = 2
+  val element2 = 21
   val element3 = 3
 
   val inputList = List(element1, element2, element3)
 
 
-  log.info(s"Sum of list = ${app.operateList(inputList,"sum",app.add)}")
+  log.info(s"Sum of list = ${app.operateList(inputList, "sum", app.add)}\n")
 
-  //log.info(s"${app.addRecursive(inputList)}")
+  log.info(s"Maximum of List = ${app.operateList(inputList, "maximum", app.maximum)}\n")
 
   log.info(s"Sum of squares = ${
     app.sum((inputNumber1, inputNumber2) => inputNumber1 * inputNumber2 +
@@ -33,4 +33,7 @@ object Application extends App {
   }\n")
 
   log.info(s"Sum of ints = ${app.sum((inputNumber1, inputNumber2) => inputNumber1 + inputNumber2, inputNumber1, inputNumber2)}\n")
+
+  log.info(s"Element in Pascal's triangle = ${app.findElementInPascalTriangle(1,3)}")
+
 }
--- src/main/scala/edu/knoldus/Operations.scala
+++ src/main/scala/edu/knoldus/Operations.scala
@@ -13,16 +13,28 @@ class Operations {
   def maximum(element1: Int, element2: Int): Int = if (element1 > element2) element1 else element2
 
   def operateList(inputList: List[Int], operation: String, function: (Int, Int) => Int): Int = {
-    def listOperation(inputList: List[Int], result: Int): Int = {
+    def listOperation(inputList: List[Int], function: (Int, Int) => Int, result: Int): Int = {
       inputList match {
-        case Nil => result
-        case head :: tail => listOperation(tail,function(head,result))
+        case Nil => throw new Exception("list is empty")
+        case head :: Nil => head
+        case head :: tail => listOperation(tail, function, function(head, result))
       }
-      operation match {
-        case "sum" => operateList(inputList,"sum", add)
-        case "product" => operateList(inputList,"product",product)
-        case "maximum" => operateList(inputList,"maximum", maximum)
-      }
+    }
+
+    operation match {
+      case "sum" => listOperation(inputList, add, 0)
+      case "product" => listOperation(inputList, product, 1)
+      case "maximum" => listOperation(inputList, maximum, 0)
+    }
+  }
+
+  def findElementInPascalTriangle(column: Int, row: Int): Int = {
+    if (column == 0 || column == row) {
+      1
+    }
+    else {
+      findElementInPascalTriangle(column - 1, row - 1) + findElementInPascalTriangle(column, row -1)
+
     }
   }
 
